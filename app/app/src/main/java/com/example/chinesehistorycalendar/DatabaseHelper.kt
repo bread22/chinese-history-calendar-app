fb9ad746eaddf4cd26ed5e6d10250b77d2c8455f
bread22 <wuqingyi22@gmail.com>
2023-05-07 17:23:37 -0400

    setup DatabaseHelper and calling it in MainActivity

---
+++ app/app/src/main/java/com/example/chinesehistorycalendar/DatabaseHelper.kt
@@ -0,0 +1,56 @@
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.content.res.AssetManager
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.io.OutputStream
+
+class DatabaseHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_NAME = "your_database_name.db"
+        private const val DATABASE_VERSION = 1
+    }
+
+    init {
+        if (!context.getDatabasePath(DATABASE_NAME).exists()) {
+            copyDatabaseFromAssets(context)
+        }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+    }
+
+    private fun copyDatabaseFromAssets(context: Context) {
+        val assetManager: AssetManager = context.assets
+        var inputStream: InputStream? = null
+        var outputStream: OutputStream? = null
+
+        try {
+            inputStream = assetManager.open(DATABASE_NAME)
+            val outputFile = context.getDatabasePath(DATABASE_NAME)
+            outputFile.parentFile?.mkdirs()
+            outputStream = FileOutputStream(outputFile)
+
+            val buffer = ByteArray(1024)
+            var length: Int
+            while (inputStream.read(buffer).also { length = it } > 0) {
+                outputStream.write(buffer, 0, length)
+            }
+
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            inputStream?.close()
+            outputStream?.flush()
+            outputStream?.close()
+        }
+    }
+}
--- app/app/src/main/java/com/example/chinesehistorycalendar/MainActivity.kt
+++ app/app/src/main/java/com/example/chinesehistorycalendar/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.chinesehistorycalendar
 
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import DatabaseHelper
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dbHelper = DatabaseHelper(this)
+
+        val db = dbHelper.readableDatabase
+        val cursor: Cursor = db.rawQuery("SELECT * FROM your_table_name", null)
+
+        while (cursor.moveToNext()) {
+            // Read data from the cursor and display it
+        }
+
+        cursor.close()
+        db.close()
     }
 }
